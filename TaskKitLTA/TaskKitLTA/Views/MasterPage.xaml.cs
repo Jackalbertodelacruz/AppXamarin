c9fa3c9631d838fac643865e34c8428d81cfcdf1
jose alberto de la cruz solano <nacho_jes2@hotmail.com>
2019-02-04 00:46:30 -0600

    Avances

---
+++ TaskKitLTA/TaskKitLTA/Views/MasterPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskKitLTA.ViewsModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TaskKitLTA.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MasterPage : ContentPage
+	{
+        public ListView ListView { get { return navigationDrawerList; } }
+        public MasterPage ()
+		{
+			InitializeComponent ();
+            BindingContext = new MasterPageViewModel();
+        }
+	}
+}
---
+++ TaskKitLTA/TaskKitLTA/ViewsModels/MasterPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskKitLTA.Models;
+using TaskKitLTA.Views;
+
+namespace TaskKitLTA.ViewsModels
+{
+    public class MasterPageViewModel : ViewModelBase
+    {
+        private List<MasterPageItem> _items;
+
+        public List<MasterPageItem> Items
+        {
+            get { return _items; }
+            set { SetProperty(ref _items, value); }
+        }
+
+        public MasterPageViewModel()
+        {
+            Items = new List<MasterPageItem>();
+            Load();
+        }
+
+        private void Load()
+        {
+            Items.Add(new MasterPageItem
+            {
+                Title = "EJERCICIO 1",
+                Icon = "l130.png",
+                Target = typeof(PendientesPage)
+            });
+
+        }
+    }
+}
